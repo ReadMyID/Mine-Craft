9f3b238dedf767990846773d5aed46dbf4a9efb6
Muchuan Go <gongmuchuan@gmail.com>
2021-07-22 17:16:45 -0700

    updated html and webserver

--- First.go
+++ First.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 type Person struct {
 	Address Address
@@ -11,9 +16,23 @@ type Address struct {
 	StreetName string
 	City       string
 }
-func Address tostring() string{
-	
+
+func (address Address) tostring() string {
+	return strconv.Itoa(+address.DoorNbr) + " " + address.StreetName + " " + address.City
 }
 func showAddress(p Person) {
 	fmt.Println(p.Address.tostring())
+}
+
+func main() {
+	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprint(rw, "Hello %q!", html.EscapeString(r.URL.Path))
+	// })
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+		http.ServeFile(rw, r, "htmls/"+r.URL.Path[1:])
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
