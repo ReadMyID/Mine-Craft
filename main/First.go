e18c019f22a0d1c11dd02868e8197845f1cfe315
Frank G <gongmuchuan@gmail.com>
2021-07-23 19:55:27 -0700

    add test.go

--- main/First.go
+++ main/First.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
 type Person struct {
 	Address Address
 }
@@ -23,16 +28,37 @@ func (address Address) tostring() string {
 func showAddress(p Person) {
 	fmt.Println(p.Address.tostring())
 }
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func fetchFile() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+		http.ServeFile(rw, r, "htmls/"+r.URL.Path[1:])
+	})
+}
 
 func main() {
 	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 	// 	fmt.Fprint(rw, "Hello %q!", html.EscapeString(r.URL.Path))
 	// })
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.URL.Path)
-		http.ServeFile(rw, r, "htmls"+r.URL.Path[1:])
-	})
+	fetchFile()
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
